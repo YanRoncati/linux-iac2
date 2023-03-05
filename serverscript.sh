8f3915780573904f6e439a0b37324557c2a2b5e6
YanRoncati <yanroncati@gmail.com>
2023-03-05 20:03:39 -0300

    first commit

---
+++ serverscript.sh
@@ -0,0 +1,14 @@
+#!/bin/BASH
+
+echo "Instalando aplicativos necessários e atualizando o sistema"
+apt-get update
+apt-get upgrade -y
+apt-get install apache2 -y
+apt-get install unzip -y
+
+echo "baixando e copiando os arquivos necessários"
+cd /tmp
+wget https://github.com/denilsonbonatti/linux-site-dio/archive/refs/heads/main.zip
+unzip main.zip
+cd linux-site-dio-main
+cp -R * /var/www/html
